fbf6189838dd175400619190f8e054f21c0d50d3
Ken Hawkins <khawkins98@gmail.com>
2020-03-04 21:03:03 +0100

    Update sass paths, autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
 var sassPaths = [
-  'bower_components/normalize.scss/sass',
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+  'node_modules/normalize.scss/sass',
+  'node_modules/foundation-sites/scss',
+  'node_modules/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
@@ -14,9 +14,7 @@ gulp.task('sass', function() {
       outputStyle: 'compressed' // if css compressed **file size**
     })
       .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
+    .pipe($.autoprefixer({}))
     .pipe(gulp.dest('css'));
 });
 
